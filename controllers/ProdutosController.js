9381313a9de3ce50429ba5bdee923e87fe4a1e89
henrique1998 <henri.rnp@gmail.com>
2020-10-31 17:50:27 -0300

    Update ProdutosController.js

--- controllers/ProdutosController.js
+++ controllers/ProdutosController.js
@@ -1,7 +1,6 @@
 const express = require ("express");
 const {Prods} = require('../db');
 const moment = require('moment');
-const jwt = require('jwt-simple');
 
 exports.registerProd = async (req, res) => {
 
