d48d8a63db0c98e919d9f9288cc9edec4ac18ebd
henrique1998 <henri.rnp@gmail.com>
2020-11-04 16:04:33 -0300

    Update apiProds.js

--- routes/api/apiProds.js
+++ routes/api/apiProds.js
@@ -6,6 +6,10 @@ const router = express.Router();
 //post novo produto
 router.post('/register', authController.registerProd);
 
+//post pesquisar produto
 router.post('/pesquisa', authController.pesquisarProd);
+
+//post listar todos os produtos
+router.post('/listar_produtos', authController.listarProds);
 
 module.exports = router;
