58827cad5f6b0ce391cb9017db370971fba9b59a
henrique1998 <henri.rnp@gmail.com>
2020-11-16 12:12:35 -0300

    Rename SuporteController.js to Suporte.js

--- controllers/Suporte.js
+++ controllers/Suporte.js
@@ -1,7 +1,14 @@
 const Trein = require('../models/SuporteTreinamento');
+const Man = require('../models/SuporteManutenção');
 
 exports.RegisterTrein = async (req, res) => {
 
     const trein = await Trein.create(req.body)
     res.json(trein)
+}
+
+exports.RegisterMan = async (req, res) => {
+
+    const man = await Man.create(req.body)
+    res.json(man)
 }
