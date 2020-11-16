29566e0502606ee34b4e1e7014db9564f3e492ca
henrique1998 <henri.rnp@gmail.com>
2020-11-16 15:16:39 -0300

    Update SuporteController.js

--- controllers/SuporteController.js
+++ controllers/SuporteController.js
@@ -2,13 +2,33 @@ const Trein = require('../models/SuporteTreinamento');
 const Man = require('../models/SuporteManutenção');
 
 exports.RegisterTrein = async (req, res) => {
-
-    const trein = await Trein.create(req.body)
-    res.json(trein)
+    const trein = await Trein.findOne({
+        where:{
+            DATA: req.body.NOME,
+            HORA: req.body.HORA
+        }
+    })
+    if(trein){
+        res.json({error: 'Horario já foi marcado por outro.'})
+    }
+    else{
+        const trein = await Trein.create(req.body)
+        res.json(trein)
+    }
 }
 
 exports.RegisterMan = async (req, res) => {
-
-    const man = await Man.create(req.body)
-    res.json(man)
+    const man = await Man.findOne({
+        where:{
+            DATA: req.body.NOME,
+            HORA: req.body.HORA
+        }
+    })
+    if(man){
+        res.json({error: 'Horario já foi marcado por outro.'})
+    }
+    else{
+        const man = await Man.create(req.body)
+        res.json(man)
+    }
 }
