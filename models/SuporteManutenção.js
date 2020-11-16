6cde3ae306c1721063f416e5246eb906b0e64ba4
henrique1998 <henri.rnp@gmail.com>
2020-11-16 14:37:15 -0300

    Update SuporteManutenção.js

--- models/SuporteManutenção.js
+++ models/SuporteManutenção.js
@@ -2,8 +2,8 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 module.exports = db.sequelize.define('suporte_manutenção', {
-        DATA: { type: Sequelize.INTEGER},
-        HORA: { type: Sequelize.INTEGER},
+        DATA: { type: Sequelize.STRING},
+        HORA: { type: Sequelize.STRING},
         NOME_CLIENTE: { type: Sequelize.STRING},
         NOME_EMPRESA: { type: Sequelize.STRING},
         CPF: { type: Sequelize.INTEGER},
