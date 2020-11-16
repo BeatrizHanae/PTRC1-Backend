44567eb56b0be800c1310eadbe1aae6854fc9fc9
henrique1998 <henri.rnp@gmail.com>
2020-11-16 12:03:28 -0300

    Update TreinamentoController.js

--- controllers/TreinamentoController.js
+++ controllers/TreinamentoController.js
@@ -2,6 +2,6 @@ const Sup = require('../models/SuporteTreinamento');
 
 exports.SupTrein = async (req, res) => {
 
-    const sup = await Sup.create(req.body)
-    res.json(sup)
+    const trein = await Trein.create(req.body)
+    res.json(trein)
 }
