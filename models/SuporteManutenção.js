a82f3792ce5e0534483b941828bf56186d9d1213
Mateus-BF <70853075+Mateus-BF@users.noreply.github.com>
2020-11-16 13:27:09 -0300

    Update SuporteManutenção.js

--- models/SuporteManutenção.js
+++ models/SuporteManutenção.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const db = require('./db')
+const db = require('../db')
 
 module.exports = db.sequelize.define('suporte_manutenção', {
         DATA: { type: Sequelize.INTEGER},
